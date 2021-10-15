40a2e22e54f377b114cf08edd10405491c01c44c
Katie Pell <57158239+katiepell42@users.noreply.github.com>
2021-10-15 14:16:44 -0700

    katie's changes to master

--- app.js
+++ app.js
@@ -10,6 +10,23 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + "/pages/main.html",)
 })
 
+app.get('/register', (req, res) => {
+  res.sendFile(__dirname + "/pages/register.html")
+})
+
+app.get('/userlist', (req, res) => {
+  res.sendFile(__dirname + "/pages/userlist.html")
+})
+
+app.get('/shelterlist', (req, res) => {
+  res.sendFile(__dirname + "/pages/shelterlist.html")
+})
+
+app.get('/shelterreg', (req, res) => {
+  res.sendFile(__dirname + "/pages/shelterreg.html")
+})
+
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
