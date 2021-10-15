c019a4e7a8c757600d95a22b27cf8de41dd8ebfc
willm62 <will.migdol@gmail.com>
2021-10-15 11:53:24 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+console.log(__dirname + "pages/index.html");
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + "/pages/main.html",)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
