3674fb6ef1fee5f4cfb228aff211d8029fad5fa7
willm0602 <will.migdol@gmail.com>
2021-10-16 23:32:02 -0700

    added search feature

--- public/scripts/userlist.js
+++ public/scripts/userlist.js
@@ -2,9 +2,11 @@ const list = $("#userStatusRows");
 
 async function getUsers()
 {
+    var users;
     await $.get("/api/user", (data, status) => {
-        console.log(data);
+        users = data;
     })
+    return users;
 }
 
 async function getID()
@@ -60,20 +62,33 @@ $(".danger").on("click", async(e) => {
 
 const search_field = $("#site-search");
 const users_list_field = $("#userStatusRows");
+const searchButton = $(".search-users-button");
 
 async function getListOfPeople()
 {
     var users = await getUsers();
+    console.log(users);
+    var phrase = search_field.val();
+    console.log("phrase is ", phrase);
+    users_list_field.empty();
+
     users.forEach((user) => {
         try {
-            var row = `<li class="userStatusRow"><img src="/static/images/avatar.png">${user.firstName} ${user.lastName} is ${user.status}</li>`
-            console.log(row);
-            users_list_field.append(row);
+            if(phrase=="" || user.firstName.includes(phrase) || user.lastName.includes(phrase) || `${user.firstName} ${user.lastName}`.includes(phrase))
+            {
+                var row = `<li class="userStatusRow"><img src="/static/images/avatar.png">${user.firstName} ${user.lastName} is ${user.status}</li>`
+                users_list_field.append(row);
+            }
         } catch (error) {
-            console.error(error);
         }
     })
 
 }
 
 getListOfPeople();
+
+console.log(searchButton);
+searchButton.on('click', async () => {
+    console.log("button clicked");
+    await getListOfPeople();
+})
